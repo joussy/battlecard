f1700ebe17a689c871138fd027dad438f6a25afd
joussy <joussyjousse@gmail.com>
2023-12-03 09:48:51 +0000

    launch settings

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,4 @@ import App from "./App.vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-createApp(App).mount("#app");
+createApp(App).mount("#app"); 
