a19956cb9139ef28a522c64b3ce87e8e444f1742
Simon JOUSSE <sjousse@hellowork.com>
2024-05-06 11:49:22 +0000

    club statistics component

--- src/App.vue
+++ src/App.vue
@@ -88,23 +88,7 @@
       </div>
       <div class="col-md-6">
         <FightCard :fightCard="fightCard"></FightCard>
-        <h3 class="mb-4">Clubs</h3>
-        <table class="table table-sm">
-          <thead>
-            <tr>
-              <th scope="col">Club</th>
-              <th scope="col">Available</th>
-              <th scope="col">Fights</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(club, index) in getClubFighters()" :key="index">
-              <td>{{ club.clubName }}</td>
-              <td>{{ club.available }}</td>
-              <td>{{ club.selected }}</td>
-            </tr>
-          </tbody>
-        </table>
+        <ClubStatistics :boxers="boxers" :fightCard="fightCard"></ClubStatistics>
       </div>
     </div>
   </div>
@@ -117,10 +101,12 @@ import { ModalityError, ModalityErrorType } from './types/modality.d'
 import { BeaModality } from './fightModality/BeaModality'
 import ModalityError from "./ModalityError.vue"
 import FightCard from "./FightCard.vue"
+import ClubStatistics from "./ClubStatistics.vue"
 export default {
   components: {
     ModalityError,
-    FightCard
+    FightCard,
+    ClubStatistics
   },
   data() {
     let ret: BoxingData = {
@@ -296,31 +282,6 @@ SERRANO	Amanda	8	F	57	Club1
     },
     getBoxerDisplayName(boxer: Boxer) {
       return `${boxer.attributes.lastName} ${boxer.attributes.firstName}`;
-    },
-    groupBy(list: any, keyGetter: (x: any) => any) : Map<any, any>{
-    const map = new Map();
-    list.forEach((item: any) => {
-         const key = keyGetter(item);
-         const collection = map.get(key);
-         if (!collection) {
-             map.set(key, [item]);
-         } else {
-             collection.push(item);
-         }
-    });
-    return map;
-  },
-    getClubFighters(): ClubFighters[] {
-      const clubs = this.groupBy(this.boxers, (x) => x.attributes.club);
-      const thisObj = this;
-      const clubFighters : ClubFighters[] = Array.from(clubs.keys()).map(function (clubKey: any) {
-        return {
-          available: clubs.get(clubKey).length,
-          selected: thisObj.fightCard.filter(f => f.boxer1.attributes.club == clubKey || f.boxer2.attributes.club == clubKey).length,
-          clubName: clubKey,
-        }
-        });
-      return clubFighters;
     },
     clear(): void {
       this.boxers = [];
---
+++ src/ClubStatistics.vue
@@ -0,0 +1,63 @@
+<template>
+  <h3 class="mb-4">Clubs</h3>
+  <table class="table table-sm">
+    <thead>
+      <tr>
+        <th scope="col">Club</th>
+        <th scope="col">Available</th>
+        <th scope="col">Fights</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(club, index) in getClubFighters()" :key="index">
+        <td>{{ club.clubName }}</td>
+        <td>{{ club.available }}</td>
+        <td>{{ club.selected }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script lang="ts">
+import { ref, Ref } from 'vue';
+import { Boxer, Gender, Fight, BoxingData, Opponent, BoxingStorage, BoxerAttributes, ClubFighters } from './types/boxing.d'
+export default {
+  props: ['fightCard', 'boxers'],
+  components: {
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    groupBy(list: any, keyGetter: (x: any) => any): Map<any, any> {
+      const map = new Map();
+      list.forEach((item: any) => {
+        const key = keyGetter(item);
+        const collection = map.get(key);
+        if (!collection) {
+          map.set(key, [item]);
+        } else {
+          collection.push(item);
+        }
+      });
+      return map;
+    },
+    getClubFighters(): ClubFighters[] {
+      const clubs = this.groupBy(this.boxers, (x) => x.attributes.club);
+      const thisObj = this;
+      const clubFighters: ClubFighters[] = Array.from(clubs.keys()).map(function (clubKey: any) {
+        return {
+          available: clubs.get(clubKey).length,
+          selected: thisObj.fightCard.filter(f => f.boxer1.attributes.club == clubKey || f.boxer2.attributes.club == clubKey).length,
+          clubName: clubKey,
+        }
+      });
+      return clubFighters;
+    }
+  },
+};
+</script>
+
+<style>
+/* Add your component-specific styles here */
+</style>
