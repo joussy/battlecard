8716dcc2a7305110546b5bced9548e9e00f25f07
joussy <joussy@joussy.com>
2025-04-26 15:33:19 +0000

    scale test button

--- src/components/selector/boxer-tile-details.component.vue
+++ src/components/selector/boxer-tile-details.component.vue
@@ -13,6 +13,38 @@
             <h5 class="ps-2 card-title fw-bold flex-fill">
                 {{ boxer.attributes.lastName }} {{ boxer.attributes.firstName }}
             </h5>
+            <div
+                class="btn btn-sm btn-outline-light me-2"
+                data-bs-toggle="modal"
+                data-bs-target="#exampleModal"
+            >
+                <Icon name="scale"></Icon>
+            </div>
+            <div class="btn btn-sm btn-outline-success">
+                <i class="bi bi-pencil"></i>
+            </div>
+        </div>
+        <!-- Modal -->
+        <div
+            id="exampleModal"
+            class="modal fade"
+            tabindex="-1"
+            aria-labelledby="exampleModalLabel"
+            aria-hidden="true"
+        >
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-body">
+                        <input
+                            id="exampleFormControlInput1"
+                            type="email"
+                            class="form-control"
+                            placeholder="name@example.com"
+                            value="12.3"
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="card mb-3">
             <div class="card shadow rounded">
