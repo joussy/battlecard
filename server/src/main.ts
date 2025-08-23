117517c2a52328cec721ccdf7185de267bf80380
joussy <simon.jousse@cegetel.net>
2025-08-23 09:23:25 +0000

    Add OpenAPI documentation generation and initial specification

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConsoleLogger, ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { writeFileSync } from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -27,6 +28,7 @@ async function bootstrap() {
       .build();
     const documentFactory = () => SwaggerModule.createDocument(app, config);
     SwaggerModule.setup('api', app, documentFactory);
+    writeFileSync('./openapi.json', JSON.stringify(documentFactory(), null, 2));
   }
 
   await app.listen(process.env.PORT ?? 3000);
