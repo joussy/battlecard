87bb8540ac5ae2d0c46425ba0ef184d7662daa5b
joussy <joussy@joussy.com>
2026-01-03 20:50:06 +0000

    bugfix date i18n

--- client/src/components/fight-card/shared-fight-card.component.vue
+++ client/src/components/fight-card/shared-fight-card.component.vue
@@ -92,7 +92,7 @@ import ShareComponent from "@/components/shared/core/share.component.vue"
 import IconComponent from "@/components/shared/core/icon.component.vue"
 import { downloadWithDom } from "@/utils/download.utils"
 
-const { t: $t } = useI18n()
+const { t: $t, locale } = useI18n()
 
 const route = useRoute()
 const showShareOffcanvas = ref(false)
@@ -102,14 +102,13 @@ const tournament = ref<SharedFightCard | null>(null)
 const accessDenied = ref(false)
 
 const tournamentDate = computed(() => {
-    //with date-fns
     return tournament.value?.tournamentDate
-        ? new Date(tournament.value.tournamentDate).toLocaleDateString("en-US", {
+        ? new Date(tournament.value.tournamentDate).toLocaleDateString(locale.value, {
               year: "numeric",
               month: "long",
               day: "numeric",
           })
-        : "Unknown Date"
+        : ""
 })
 
 onMounted(async () => {
